5c845cba7a36788344f8cd8d2ebf50760fdd17c4
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-08 18:44:50 +0200

    Labelling of coordinates

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class CubeEditor : MonoBehaviour
 {
     [Range(0.1f, 20f)]
     [SerializeField] float gridSize = 10f;
 
+    TextMesh textMesh;
+
     private void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * 10f;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * 10f;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
 
         transform.position = new Vector3(snapPos.x, 0, snapPos.z);
     }
