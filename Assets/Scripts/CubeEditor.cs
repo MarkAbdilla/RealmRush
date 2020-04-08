ca7126bac962a06e41370719e34243a5ab31fc09
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-08 18:10:18 +0200

    Editor snapping implemented

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+    [Range(0.1f, 20f)]
+    [SerializeField] float gridSize = 10f;
+
+    private void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * 10f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * 10f;
+
+        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+    }
+
+}
