c768b3a16e204105eddde2f57cb68025c7634146
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-09 20:35:51 +0200

    Preliminary pathfinding tests

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -9,12 +9,35 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Waypoint endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
-    // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColourStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            print("Exploring " + explorationCoordinates);
+            try
+            {
+                grid[explorationCoordinates].SetTopColour(Color.yellow);
+            }
+            catch
+            {
+
+            }
+        }
     }
 
     private void LoadBlocks()
