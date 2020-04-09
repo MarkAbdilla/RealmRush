0406590a25779cade236c678aeef0923c98b5f0b
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-09 18:46:21 +0200

    Addition of dictionary for pathfinding

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            bool isOverlapping = grid.ContainsKey(gridPos);
+            if (isOverlapping)
+            {
+                Debug.LogWarning("Overlapping block found " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
