4045df134b9a2121e9d8be9990f234f2c654b01c
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-14 21:23:28 +0200

    Towers only shoot when enemy is within range

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,38 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 10f;
+    [SerializeField] ParticleSystem projectileParticle;
 
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+        }
+        else
+        {
+            ShootEnemy(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position);
+        if (distanceToEnemy <= attackRange)
+        {
+            ShootEnemy(true);
+        }
+        else
+        {
+            ShootEnemy(false);
+        }
+    }
+
+    private void ShootEnemy(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
