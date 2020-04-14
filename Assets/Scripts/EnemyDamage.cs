77cb7f5a2fc8a76f972e002a39e55e34d1e521da
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-14 20:17:21 +0200

    Towers now damage and kill enemies

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 10f;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        hitPoints = hitPoints - 1; //to consider different damage values according to tower
+
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
