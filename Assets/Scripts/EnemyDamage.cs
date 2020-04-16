4b1367cd710f3d865488dcbe44241a865f170872
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-16 18:25:37 +0200

    New particle effects added for enemy hits and enemy death

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] float hitPoints = 10f;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem deathParticlePrefab;
 
     private void OnParticleCollision(GameObject other)
     {
-        hitPoints = hitPoints - 1; //to consider different damage values according to tower
+        ProcessHit();
+        ProcessDeath();
+    }
 
+    private void ProcessHit()
+    {
+        hitPoints = hitPoints - 1; //to consider different damage values according to tower
+        hitParticlePrefab.Play();
+    }
+
+    private void ProcessDeath()
+    {
         if (hitPoints <= 0)
         {
+            var deathParticleFX = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
+            deathParticleFX.Play();
             Destroy(gameObject);
+            Destroy(deathParticleFX, 1f);
         }
     }
 }
