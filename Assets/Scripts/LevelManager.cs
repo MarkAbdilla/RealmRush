fadf4c17289aac178c00bc6993bc90839b9ae505
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-21 18:30:06 +0200

    Preliminary Main Menu setup

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void FirstLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
